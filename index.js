f3b3b1e52947f413d0ce9d234ebd13f7c5404919
Nic Pismiris <Nicolas.Pismiris@gmail.com>
2023-04-22 13:27:47 +0000

    Added Multiple Password Generation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const generatePasswordBtn = document.getElementById('generate-password-btn');
-const generateSecurePasswordBtn = document.getElementById('generate-secure-password-btn');
-const passwordDisplay = document.getElementById('password-display');
+const generatePasswordBtn = document.getElementById("generate-password-btn");
+const generateSecurePasswordBtn = document.getElementById(
+  "generate-secure-password-btn"
+);
+const passwordDisplay = document.getElementById("password-display");
 const copyPasswordButton = document.getElementById("copy-password");
+const numPasswordsInput = document.getElementById("numPasswords");
 
-const consonants = 'BCDFGHJKLMNPQRSTVWXYZ';
-const capital = consonants.split('');
-const consonant = consonants.toLowerCase().split('');
-const vowel = 'aeiou'.split('');
-const number = '123456798'.split('');
+const consonants = "BCDFGHJKLMNPQRSTVWXYZ";
+const capital = consonants.split("");
+const consonant = consonants.toLowerCase().split("");
+const vowel = "aeiou".split("");
+const number = "123456798".split("");
 
 function generate4LogicPassword() {
   let pw = capital[Math.floor(Math.random() * capital.length)];
@@ -16,10 +19,13 @@ function generate4LogicPassword() {
   pw += vowel[Math.floor(Math.random() * vowel.length)];
   pw += consonant[Math.floor(Math.random() * consonant.length)];
   pw += vowel[Math.floor(Math.random() * vowel.length)];
-  pw += Array.from({ length: 2 }, () => number[Math.floor(Math.random() * number.length)]).join('');
-  pw += '!';
+  pw += Array.from(
+    { length: 2 },
+    () => number[Math.floor(Math.random() * number.length)]
+  ).join("");
+  pw += "!";
 
-  pw = pw.replace(/\s/g, '');
+  pw = pw.replace(/\s/g, "");
 
   return pw;
 }
@@ -31,48 +37,51 @@ function generate4LogicSecurePassword() {
   pw += vowel[Math.floor(Math.random() * vowel.length)];
   pw += consonant[Math.floor(Math.random() * consonant.length)];
   pw += vowel[Math.floor(Math.random() * vowel.length)];
-  pw += '@';
-  pw += Array.from({ length: 4 }, () => number[Math.floor(Math.random() * number.length)]).join('');
-  pw += '!';
+  pw += "@";
+  pw += Array.from(
+    { length: 4 },
+    () => number[Math.floor(Math.random() * number.length)]
+  ).join("");
+  pw += "!";
 
-  pw = pw.replace(/\s/g, '');
+  pw = pw.replace(/\s/g, "");
 
   return pw;
 }
 
-document.getElementById("copy-password").addEventListener("click", function() {
-  const password = passwordDisplay.innerText;
-  if (!password) {
-    return;
-  }
-  navigator.clipboard.writeText(password)
-    .then(() => {
-      console.log('Password copied to clipboard');
-    })
-    .catch(err => {
-      console.error('Error copying password: ', err);
-    });
-});
-
 function copyPasswordToClipboard() {
   const password = passwordDisplay.innerText;
-  navigator.clipboard.writeText(password).then(function() {
+  navigator.clipboard.writeText(password).then(function () {
     const notification = document.getElementById("notification");
     notification.classList.remove("hidden");
-    setTimeout(function() {
+    setTimeout(function () {
       notification.classList.add("hidden");
     }, 2000);
   });
 }
 
-generatePasswordBtn.addEventListener('click', () => {
-  const password = generate4LogicPassword();
-  passwordDisplay.innerText = password;
+generatePasswordBtn.addEventListener("click", () => {
+  let numPasswords = numPasswordsInput.value || 1;
+  let passwordList = [];
+
+  for (let i = 0; i < numPasswords; i++) {
+    const password = generate4LogicPassword();
+    passwordList.push(password);
+  }
+
+  passwordDisplay.innerText = passwordList.join("\n");
 });
 
-generateSecurePasswordBtn.addEventListener('click', () => {
-  const password = generate4LogicSecurePassword();
-  passwordDisplay.innerText = password;
+generateSecurePasswordBtn.addEventListener("click", () => {
+  let numPasswords = numPasswordsInput.value || 1;
+  let passwordList = [];
+
+  for (let i = 0; i < numPasswords; i++) {
+    const password = generate4LogicSecurePassword();
+    passwordList.push(password);
+  }
+
+  passwordDisplay.innerText = passwordList.join("\n");
 });
 
 copyPasswordButton.addEventListener("click", copyPasswordToClipboard);
